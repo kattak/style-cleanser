ea59097f2e5843fa1d5d0bfd00542efe71b2bdd3
kattak <katrina.devaney@gmail.com>
2017-01-26 23:16:32 -0800

    posting factoid with text and url works :white_check_mark:

--- content.js
+++ content.js
@@ -1,11 +1,11 @@
 // Called when the user clicks on the browser action.
  chrome.browserAction.onClicked.addListener(function(tab) {
    chrome.tabs.executeScript({
-     code: 'window.getSelection().toString();'
+     code: '[window.getSelection().toString(), window.location.href]'
    }, function(selection){
      console.log(selection[0])
      console.log(Object.getOwnPropertyNames(selection[0]));
-     $.post( "http://localhost:3000/factoids", {factoid:{text: selection[0]}} );
+     $.post( "http://localhost:3000/factoids", {factoid:{text: selection[0][0], url:selection[0][1]}} );
    });
 
    chrome.tabs.executeScript({

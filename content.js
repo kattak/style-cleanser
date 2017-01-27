8a174296d936c54617caba42d009ff9bd706aa47
kattak <katrina.devaney@gmail.com>
2017-01-26 22:47:58 -0800

    Clean up, posting factoid still works

--- content.js
+++ content.js
@@ -1,21 +1,3 @@
-// // Called when the user clicks on the browser action.
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//   chrome.tabs.executeScript({
-//     // file: 'color-change.js',
-//     code: 'window.getSelection().toString();'
-//    })
-//
-//   console.log('waka flaka')
-//   // No tabs or host permissions needed!
-//   // chrome.tabs.executeScript({
-//   //   code: 'document.body.style.backgroundColor="red"'
-//   // });
-//
-//
-//
-// });
-
-
 // Called when the user clicks on the browser action.
  chrome.browserAction.onClicked.addListener(function(tab) {
    chrome.tabs.executeScript({
@@ -23,22 +5,15 @@
    }, function(selection){
      console.log(selection[0])
      console.log(Object.getOwnPropertyNames(selection[0]));
-     // selNode = selection.anchorNode();
-     // console.log(selNode);
-
-     //try making post request to localhost rails app
      $.post( "http://localhost:3000/factoids", {factoid:{text: selection[0]}} );
-    //
-    //  $.post( "ajax/test.html", function( data ) {
-    //    $( ".result" ).html( data );
-    //  });
-
-    //  chrome.tabs.create({
-    //    url: "http://localhost:3000/factoids?text=".concat(selection[0])
-    //  })
    });
-   // No tabs or host permissions needed!
-   // chrome.tabs.executeScript({
-   //   code: 'document.body.style.backgroundColor="red"'
-   // });
+
+   chrome.tabs.executeScript({
+     code: 'console.log("hi");'
+   }, function(selection){
+    //  console.log(selection[0])
+    //  console.log(Object.getOwnPropertyNames(selection[0]));
+    //  $.post( "http://localhost:3000/factoids", {factoid:{text: selection[0]}} );
+   });
+
  });
